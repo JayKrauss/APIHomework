7577a5665d9cd43e3424983cdfe669b1aaa842b4
Jay <paxformatus@gmail.com>
2018-06-13 13:54:37 -0400

    first working commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,5 @@
 //array for starting shows and added shows
-var terms = ['Simpsons', 'Family Guy', 'Futurama', 'American Dad', 'Cleveland Show', 'Archer', 'Ren and Stimpy', 'Cat Dog', 'Samurai Jack'];
+var terms = ['Simpsons', 'Family Guy', 'Futurama', 'American Dad', 'Rick and Morty', 'Archer', 'Ren and Stimpy', 'Cat Dog', 'Samurai Jack'];
 
 //function for rendering the buttons in the array
 function renderButtons() {
@@ -9,7 +9,7 @@ function renderButtons() {
 
       var name = terms[i];
 
-      var buttonCurrent = $('<button class="btn btn-primary">' + name + '</button>').attr({name: terms[i]});
+      var buttonCurrent = $('<button class="btn btn-danger" id="show-button">' + name + '</button>').val(terms[i]);
 
       $('#buttons').append(buttonCurrent);
 
@@ -17,41 +17,63 @@ function renderButtons() {
   }
 
   //Click event for adding shows to the array
-  $("#add-show").on("click", function(event) {
+  $(document).on('click', '#add-show', function(event) {
 
     event.preventDefault();
 
     var showText = $("#show-input").val().trim();
     console.log(showText);
 
-    terms.push(showText);
+    for (i = 0; i < terms.length; i++){
 
-    renderButtons();
+      if (showText === [i]){
+        alert('You cant add the same show twice!')
+
+      }
+
+      else if (showText != [i]) {
+        terms.push(showText);  
+        renderButtons();
+        $('#show-input').clear();
+      }
+    }
+
   });
 
-  //click event for generating GIFs
-$("button").on("click", function() {
-    var term = $(this).attr("name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      term + "&api_key=dc6zaTOxFJmzC&limit=10";
+  //Gif generation
+  $(document).on("click", "#show-button", function() {
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        var results = response.data;
+      $('#display').html('');
 
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div class='item'>");
+        var show = $(this).attr('value');
+          console.log(show);
 
-          var termImage = $("<img>");
-          termImage.attr("src", results[i].images.fixed_height.url);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          show + "&api_key=dc6zaTOxFJmzC&limit=9";
+        
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div class='col-md-4'>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+  
+                var showImage = $("<img>");
 
-          gifDiv.prepend(termImage);
+                showImage.attr("src", results[i].images.fixed_height.url);
+  
+                gifDiv.append(p);
+                gifDiv.append(showImage);
 
-          $("#display").prepend(gifDiv);
-        }
+                $("#display").prepend(gifDiv);
+              }
+            }
+          });
       });
-  });
+    
 
